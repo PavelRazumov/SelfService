2a76d8fb8d3bb5369baa0674ef767d9bf041ff34
pawra <paw.razumow@yandex.ru>
2019-12-24 19:37:38 +0300

    lala

---
+++ DomainModel/SspCartItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelfServiceTerminal.DomainModel
+{
+    public class SspCartItem
+    {
+        private SspMenuItem menuItem;
+        public int quantity;
+
+        public SspCartItem(SspMenuItem menuItem)
+        {
+            MenuItem = menuItem;
+            Quantity = 1;
+        }
+        public SspMenuItem MenuItem
+        {
+            get
+            {
+                return menuItem;
+            }
+
+            set
+            {
+                menuItem = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+
+            set
+            {
+                quantity = value;
+            }
+        }
+
+    }
+}
---
+++ Services/SspPaymentService.cs
@@ -0,0 +1,24 @@
+﻿using SelfServiceTerminal.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelfServiceTerminal.Services
+{
+    public class SspPaymentService
+    {   
+        public SspOrderStage statusOrder;
+        
+        public SspPaymentService()
+        {
+            statusOrder = new SspOrderStage();
+        }
+        public void SendOnPayment(SspOrder order)
+        {
+           if (order.orderStage == SspOrderStage.WaitForPayment)
+            {
+                // send payment
+            }
+        }
+    }
+}
